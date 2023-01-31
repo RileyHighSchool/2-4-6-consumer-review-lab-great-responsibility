4b5ed59c8d9ce4dbf8da5440c3510ddf5ba42b6a
robert85827 <robert85827@students.sbcsc.k12.in.us>
2023-01-31 13:43:58 -0500

    created openEndedMain file

---
+++ openEnded/openEndedMain.java
@@ -0,0 +1,13 @@
+package openEnded;
+
+public class openEndedMain {
+    public static void main(String[] args) {
+        
+    } 
+    public static String TwoDimArray(int x, int y, String character){
+        for (int i = 0; i > x; i++){
+            
+        }
+    }
+
+}

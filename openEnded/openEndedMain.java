6741729375414829e1c19b66d29340c2166d692e
robert85827 <robert85827@students.sbcsc.k12.in.us>
2023-02-01 13:33:58 -0500

    created replaceEveryOther()

--- openEnded/openEndedMain.java
+++ openEnded/openEndedMain.java
@@ -2,12 +2,17 @@ package openEnded;
 
 public class openEndedMain {
     public static void main(String[] args) {
-        
-    } 
-    public static String TwoDimArray(int x, int y, String character){
-        for (int i = 0; i > x; i++){
-            
-        }
+        System.out.println(replaceEveryOther("the quick brown fox jumped over the lazy dog", "!"));
     }
-
+    public static String replaceEveryOther(String string, String character){
+        String output = "";
+        for(int i = 0; i < string.length(); i++){
+            if (i % 2 == 0){
+                output += string.substring(i, i+1);
+            } else {
+                output += character;
+            }
+        }
+        return output;
+    }
 }
